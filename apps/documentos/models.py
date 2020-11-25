57a07ea206d73dcdb944bc9ffeb7b31174e3bce3
diegoafaguiar <diegoafaguiar@gmail.com>
2020-11-24 23:44:52 -0200

    Configurando os relacionamentos

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     nome = models.CharField(max_length=80, help_text="Nome do documento")
+    pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
